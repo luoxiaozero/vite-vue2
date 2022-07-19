f102fc87b49ccdda99ffe538f7e378bfe843703e
luoxiao <luoxiaozero@163.com>
2022-07-19 17:31:01 +0800

    init: 添加模板

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import vue from "@vitejs/plugin-vue2";
+import legacy from "@vitejs/plugin-legacy";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+    plugins: [
+        vue(),
+        legacy({
+            targets: ["defaults", "IE 10"],
+        }),
+    ],
+});
